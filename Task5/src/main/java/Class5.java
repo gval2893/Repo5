de19f3e6b0dc1fd80f3254c81d88b19cdbcc9abd
gval2893 <gangal.valentina28@gmail.com>
2020-10-29 03:01:36 +0200

    Was added the sum function

--- Task5/src/main/java/Class5.java
+++ Task5/src/main/java/Class5.java
@@ -1,2 +1,14 @@
 public class Class5 {
-}
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int c = a+b;
+
+        System.out.println(c);
+    }
+
+    }
+
+
+
+
