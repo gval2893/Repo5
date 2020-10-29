21e8955f85561279c993d7e0e3051ce6459b9ee6
gval2893 <gangal.valentina28@gmail.com>
2020-10-29 04:41:55 +0200

    The adding commit 2

--- Task5/src/main/java/Class5.java
+++ Task5/src/main/java/Class5.java
@@ -2,11 +2,13 @@ public class Class5 {
 
     public void sayHi(String firstGuest) {
         System.out.println("Добрый вечер, " + firstGuest + ", как ваши дела?");
+        System.out.println("Hi");
     }
 
     public void sayHi(String firstGuest, String secondGuest) {
         System.out.println("Добрый вечер, " + firstGuest + ", " + secondGuest + ", как ваши дела?");
     }
+
 
 
 }
